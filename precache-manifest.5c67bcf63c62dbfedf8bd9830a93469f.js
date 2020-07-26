f1cb72ee3cd707526bc8b4356ee45e8a0e096bcd
Shubhankar <dimrishubhi@gmail.com>
2020-07-26 23:28:21 +0530

    Updates

---
+++ precache-manifest.5c67bcf63c62dbfedf8bd9830a93469f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a6272b8e8cf6c2d202ea388d693001",
+    "url": "/dimritium/markdown_previewer/index.html"
+  },
+  {
+    "revision": "a742c254a67cf3761e8d",
+    "url": "/dimritium/markdown_previewer/static/css/2.2ff3ccb9.chunk.css"
+  },
+  {
+    "revision": "b2dcfbcca9e9c4885efc",
+    "url": "/dimritium/markdown_previewer/static/css/main.80041b41.chunk.css"
+  },
+  {
+    "revision": "a742c254a67cf3761e8d",
+    "url": "/dimritium/markdown_previewer/static/js/2.da37e847.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dimritium/markdown_previewer/static/js/2.da37e847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dcfbcca9e9c4885efc",
+    "url": "/dimritium/markdown_previewer/static/js/main.2677a7a6.chunk.js"
+  },
+  {
+    "revision": "6326c4949e9e4dd326ff",
+    "url": "/dimritium/markdown_previewer/static/js/runtime-main.7de16f56.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dimritium/markdown_previewer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dimritium/markdown_previewer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dimritium/markdown_previewer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dimritium/markdown_previewer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dimritium/markdown_previewer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
